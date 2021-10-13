b5f1ab7c608f85d9365a992ee545afda4d6a2ec3
Daryl Tng <42925677+mrdarylguy@users.noreply.github.com>
2021-10-13 16:37:59 +0800

    Installed miniforge

---
+++ 2D Heat equation/Oven.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pylot as plt
+from matplotlib import animation
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import PillowWriter
+from matplotlib import cm
+from skimage import color
+from skimage import io 
+import numba
+from numba import jit
+
