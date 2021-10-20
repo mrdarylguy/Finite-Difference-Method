622716c3e5a068a646255e070e720756446792ba
Daryl Tng <42925677+mrdarylguy@users.noreply.github.com>
2021-10-20 23:32:47 +0800

    Move to new folder

--- Ocean Model/ocean_model.jl
+++
@@ -1,9 +0,0 @@
-"""
-This code deals with burgers equation in fluid mechanics.
-
-I tried to follow the lesson from the Julia Youtube channel. 
-
-"Building an ocean model from scratch | Week 13 | MIT 18.S191 Fall 2020 | Henri Drake" 
-
-"""
-
